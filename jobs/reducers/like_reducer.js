add1b475cddc24bfbfb5052ea154c642edf19f20
Param Jain <param_jain@icloud.com>
2018-09-30 22:07:39 +0530

    Liked Jobs are now Persistant
    Using redux-persist for Persistant Data so that data doesn't wipe out once a device reboots or loses net connection.

--- jobs/reducers/like_reducer.js
+++ jobs/reducers/like_reducer.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-
+import { REHYDRATE } from 'redux-persist/constants';
 import {
     LIKE_JOB,
     CLEAR_LIKED_JOBS
@@ -7,6 +7,9 @@ import {
 
 export default function (state = [], action) {
     switch (action.type) {
+        case REHYDRATE:
+            return action.payload.likedJobs||[];
+
         case LIKE_JOB:
             return _.uniqBy([
                 action.payload, ...state
--- jobs/screens/SettingsScreen.js
+++ jobs/screens/SettingsScreen.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, AsyncStorage } from 'react-native';
 import { connect } from 'react-redux';
 import { clearLikedJobs } from '../actions/job_actions';
 import { Button } from 'react-native-elements';
 
 class SettingsScreen extends Component {
+
+    onButtonPress = () => {
+        AsyncStorage.removeItem('fb_token');
+        this.props.navigation.navigate('auth');
+    }
 
     render() {
         return (
@@ -15,7 +20,16 @@ class SettingsScreen extends Component {
                    icon={{ name: 'delete-forever' }}
                    backgroundColor="#F44336"
                    onPress={this.props.clearLikedJobs}
-               /> 
+                   style = {{ marginTop: 30 }}
+               />
+               <Button
+                   title="Log Out"
+                   large
+                   icon={{ name: 'delete-forever' }}
+                   backgroundColor="#A23B56"
+                   onPress={this.onButtonPress}
+                   style = {{ marginTop: 30 }}
+               />  
             </View>
         );
     }
